30f58f3160a5e0040eb059b17099be914cea7f8c
naufal-abdillah <abdillahnfl@gmail.com>
2021-11-06 19:16:55 +0700

    Interface, struct, method experiment

--- main.go
+++ main.go
@@ -8,4 +8,44 @@ import (
 func main() {
 	fmt.Println("Printing in main")
 	cmd.Run()
+	var model_s car
+	model_s.brand = "tesla"
+	model_s.price = 100000
+	var redmi phone
+	redmi.brand = "mi"
+	redmi.price = 100
+	redmi.ram = 4
+
+	// model_s car
+	model_s.Printprice()
+	redmi.Printprice()
+	redmi.Buy()
+
+	var birthdaypresent device
+	birthdaypresent = phone{"samsung", 1000, 16}
+	birthdaypresent.Printprice()
+
+}
+
+type device interface {
+	Printprice()
+}
+type car struct {
+	brand string
+	price int
+}
+type phone struct {
+	brand string
+	price int
+	ram   int
+}
+
+func (c car) Printprice() {
+	fmt.Printf("Price of the %s is $%d\n", c.brand, c.price)
+}
+func (p phone) Printprice() {
+	fmt.Printf("Price of the %s is $%d\n", p.brand, p.price)
+}
+func (p phone) Buy() {
+	fmt.Printf("Do you want to by the %s\n", p.brand)
 }
